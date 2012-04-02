d4bcea7efe1e88dc718bff95e886706634cdb1e0
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2012-04-02 02:21:13 -0400

    put some validation on the Mileage starting/ending/trip relation

--- app/models/mileage.rb
+++ app/models/mileage.rb
@@ -1,2 +1,26 @@
 class Mileage < ActiveRecord::Base
+  before_validation_on_create :fix_ending_and_trip
+  validates_numericality_of :starting, :greater_than => 0
+
+  def ending
+    read_attribute(:ending) || self.starting
+  end
+  def trip
+    read_attribute(:trip) || (self.ending - self.starting)
+  end
+
+  private
+
+  def fix_ending_and_trip
+    if read_attribute(:ending).nil?
+      if read_attribute(:trip).nil?
+        self.ending = self.starting
+      else
+        self.ending = self.starting + self.trip
+      end
+    end
+    if read_attribute(:trip).nil?
+      self.trip = self.ending - self.starting
+    end
+  end
 end
